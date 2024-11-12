bde318aa22718a85fc4201984aa60cfde70e7c22
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2024-11-12 13:58:08 +0530

    complete chapter03 by creating and printing variables

---
+++ chapter03/variables.rb
@@ -0,0 +1,9 @@
+character_name = "binayak"
+his_hobby = "playing game"
+friend = "ayrav"
+
+puts ("his name is " + character_name)
+puts ("his hobby is " + his_hobby)
+puts ("his friend name is " + friend)
+
+# in ruby, standard convention for creating varibles is like if your varible name contains more than one word then you can use underscore in between them
