996adea59881c8b37305eecacced5e2265030a61
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2024-11-12 13:50:52 +0530

    complete chapter02 by printing a triangle shape

---
+++ chapter02/drawAShape.rb
@@ -0,0 +1,25 @@
+print "   /|"
+print "  / |"
+print " /  |"
+print "/___|"
+
+puts " "
+
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
+
+# print function print the data without new line
+# puts function print the data with new line
+
+# using print function the following code look like ->
+
+# /|  / | /  |/___|
+
+# using puts function the following code look like ->
+
+#     /|
+#    / |
+#   /  |
+#  /___|
