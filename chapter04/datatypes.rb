1520857f611f7ca3757a7c61521bfdd2c963f050
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2024-11-12 14:13:29 +0530

    complete chapter04 by writing different datatypes

---
+++ chapter04/datatypes.rb
@@ -0,0 +1,28 @@
+a = "binayak"
+
+puts a.class # a is a string type of data => String 
+
+
+b = 25
+
+puts b.class # b is a int type of data => Integer 
+
+c = 45.678 
+
+puts c.class # c is a float type of data => Float 
+
+d = true
+
+puts d.class # d is a boolean type of data => TrueClass
+
+e = false
+
+puts e.class # e is a boolean type of data => FalseClass
+
+f = nil
+
+puts f.class # f is a boolean type of data => NilClass
+
+# in ruby, nil means nothing , no value
+
+# inorder to check type of variable we can write "<variable_name>.class"
