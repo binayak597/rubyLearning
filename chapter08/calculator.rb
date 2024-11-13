f44106856a15f94fe362bfb9c05305cc85e9b4c5
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2024-11-13 12:44:14 +0530

    complete chapter08 by built a calculator app

---
+++ chapter08/calculator.rb
@@ -0,0 +1,26 @@
+# approach1
+
+
+# puts "Enter number1: "
+# num1 = gets
+
+# puts "Enter number2: "
+# num2 = gets
+
+
+# result = num1.to_i + num2.to_i
+
+# puts result
+
+# approach2
+
+puts "Enter number1: "
+num1 = gets.to_i
+
+puts "Enter number2: "
+num2 = gets.to_i
+
+
+result = num1 + num2
+
+puts result
