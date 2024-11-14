969ff65c4a87aaa06beebb6048c7f8504c3a29fe
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2024-11-14 13:18:34 +0530

    complete chapter11 by learing about funtions

---
+++ chapter11/function.rb
@@ -0,0 +1,21 @@
+def sayHi
+  puts "hi"
+end
+
+sayHi
+
+def sayHello()
+  puts "Hello"
+end
+
+sayHello()
+
+def sayYourName(name)
+  puts "hello #{name}"
+end
+
+sayYourName("Binayak")
+
+# in ruby, if you don't have any parameter in function
+# we don't need to use "()" even in function definition or in function call (optionally), if you want you can use
+
---
+++ chapter11/function_with_return.rb
@@ -0,0 +1,11 @@
+
+def sum(a, b)
+
+  result = a + b
+  return result
+
+end
+
+ans = sum(5, 7)
+
+puts ans
