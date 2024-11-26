1b90c3fdef05a495d2f2cf32351c8c0feaa80136
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2024-11-27 00:48:02 +0530

    complete chapter20 by familiar with irb concept in ruby

---
+++ chapter21/irb.rb
@@ -0,0 +1,11 @@
+# irb ->  stands for interactive ruby 
+
+# when we generally install ruby, irb automatically install with it
+
+#basically if you want to use ruby from command line like from terminal or command prompt you can use irb
+
+#just type irb and it will open for you REPL in your terminal
+
+#then, any valid ruby code you can run directly from your terminal
+
+#to check irb version use -> irb -v
